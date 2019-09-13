7083cc8c4308ba2980ddd5bc6c84b98faf4c3c8c
Pushtaryk Oleksii <pushtarik@gmail.come>
2019-09-13 18:17:12 +0300

    Updates

---
+++ precache-manifest.821e1ae5c9174a02cdf5a35ce3cf7f12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca009c7da4ad47b2acbe90286fde25f",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "df03d8b51b2a7fee612f",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.90cb2fb1.chunk.css"
+  },
+  {
+    "revision": "219bf664eafd0e21ae27",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.5687fb6c.chunk.js"
+  },
+  {
+    "revision": "df03d8b51b2a7fee612f",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.b7b6a104.chunk.js"
+  },
+  {
+    "revision": "249656bcd6aee9fe97fe",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.16247d91.js"
+  }
+]);
